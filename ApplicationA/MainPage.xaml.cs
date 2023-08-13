bfe120230330a13013d6a2093350d7c045514086
Sam <ullrichsam@gmail.com>
2023-08-12 23:29:43 -0700

    Revert "Connected buttons to initiate server and streaming. Added a temp label to view data being sent through."
    This reverts commit 4f811bd0b7841802c8b6e48e6c04c34fb4b0b0de.

--- ApplicationA/MainPage.xaml.cs
+++ ApplicationA/MainPage.xaml.cs
@@ -1,48 +1,24 @@
-﻿using EmbedIO;
-using EmbedIO.WebApi;
-
-namespace ApplicationA
+﻿namespace ApplicationA
 {
     public partial class MainPage : ContentPage
     {
-        private WebServer server;
+        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
-        private void OnStartServerClicked(object sender, EventArgs e)
+
+        private void OnCounterClicked(object sender, EventArgs e)
         {
-            if (server == null)
-            {
-                var url = "http://localhost:9696/";
-                server = CreateWebServer(url);
-                server.RunAsync();
-                StartServerBtn.Text = "Server Started";
-            }
-        }
+            count++;
 
-        private void OnStartStreamingClicked(object sender, EventArgs e)
-        {
-            if (server != null)
-            {
-                DataWebSocketModule.StartStreaming();
-                StartStreamingBtn.Text = "Streaming Started";
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
 
-                TimestampLabel.Text = "Timestamp: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-            }
-        }
-
-        private static WebServer CreateWebServer(string url)
-        {
-            var server = new WebServer(o => o
-            .WithUrlPrefix(url)
-            .WithMode(HttpListenerMode.EmbedIO))
-            .WithWebApi("/api", m => m
-                .WithController<StreamingController>())
-            .WithModule(new DataWebSocketModule("/data"));
-
-            return server;
+            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
